9ddb4f23820d1a517f227ca43ea22d8b880fff04
Lucas <lucas.barreto13@hotmail.com>
2022-01-06 19:26:27 -0300

    Adicionar arquivos de projeto.

---
+++ data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercicioGetSet
+{
+    public class Data
+    {
+        private int mes;
+        private bool mesvalido;
+        public int GetMes()
+        {
+            return this.mes;
+        }
+
+        public void SetMes(int mes)
+        { 
+            if(mes > 0 && mes <= 12)
+            {
+                this.mes = mes;
+                this.mesvalido = true;
+
+            }
+        
+        }
+
+        public void ApresentarMes()
+        {
+            if(this.mesvalido)
+            {
+                Console.WriteLine(this.mes);
+            }
+            else
+            {
+                Console.WriteLine("Mês inválido!");
+            }
+        }
+
+    }
+}
+
+
+
